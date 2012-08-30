7b1626d433a472bc315c8e1c4217ac3c990e961c
Thierry Passeron <thierry.passeron@gmail.com>
2012-08-30 18:46:13 +0200

    init1

--- MCCGalleryViewDemo/MCCViewController.m
+++ MCCGalleryViewDemo/MCCViewController.m
@@ -145,11 +145,7 @@
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-  if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-      return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
-  } else {
-      return YES;
-  }
+  return YES;
 }
 
 @end
